3f6a95d3ebe54407b943d14227f3c3c51468dab2
Curvi55 <59000495+Curvi55@users.noreply.github.com>
2023-05-27 17:42:35 -0600

    screens creadas // archivo barril actualizado

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfiteScrollScreen extends StatelessWidget {
+  static const name = 'infinite _screen';
+
+  const InfiteScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
